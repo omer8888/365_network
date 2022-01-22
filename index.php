6360ec2de4363e0b81c0f367f117b3997d2bc681
omer <omernaim8@gmail.com>
2022-01-22 23:49:23 +0200

    main box - wip

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 //session_destroy();
 
 ?>
-    <div class="user_details column">
+    <div class="user_details_box column">
         <img src="<?php echo $user['profile_pic'];?>">
 
-        <div class="user_details_text">
+        <div class="user_details_box_text">
             <b><?php echo $user['first_name']." ".$user['last_name'] ;?></b>
             <br>
             <?php  echo "Total posts : ".$user['num_posts'].""; ?>
@@ -14,6 +14,14 @@
             <br>
             <?php  echo "Since : ".$user['signup_date']."";  ?>
         </div>
+    </div>
+
+    <div class="main_box column">
+            <form class="post_form" action=index.php" method="POST">
+                <textarea name=post_text" id="post_text" placeholder="What do you want to say?"></textarea>
+                <input type="submit" name="post" id="post_button" value="Post">
+                <hr>
+            </form>
     </div>
 
 </div> <!-- /wrapper -->
