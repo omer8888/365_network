566460876fa653eb1b2331cda8d1fde4e9d4c633
omer <omernaim8@gmail.com>
2022-09-03 10:51:52 +0300

    domain logic

--- resources/config.php
+++ resources/config.php
@@ -9,7 +9,7 @@ defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASS") ? null : define("DB_PASS", "");
 defined("DB_NAME") ? null : define("DB_NAME", "365_db");
-defined("domain") ? null : define("DOMAIN", "");
+defined("DOMAIN") ? null : define("DOMAIN", "");
 
 
 $connection = null;
